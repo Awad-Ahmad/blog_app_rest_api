5e4284cecdfa9e45c158d8ccc3c08c078966ad32
Ahmad Awad <ahmadmohanasawad@gmail.com>
2022-08-28 13:39:03 +0300

    a

--- api/routes/bookmark.js
+++ api/routes/bookmark.js
@@ -4,6 +4,6 @@ const bookmarkRouter=express.Router()
 const bookmarkController=require('../controllers/bookmark')
 bookmarkRouter.post("/add-bookmark/:blogId",auth,bookmarkController.add_bookmark),
 bookmarkRouter.get("/get-bookmark",auth,bookmarkController.get_all_user_bookmark)
-bookmarkRouter.delete("/delete-bookmark/:bookmarkId",auth,bookmarkController.delete_bookmark)
+bookmarkRouter.delete("/delete-bookmark/:blogId",auth,bookmarkController.delete_bookmark)
 
 module.exports=bookmarkRouter
